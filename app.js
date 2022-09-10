2daf3cd5171c4664310555b632f4ed618371c246
projectperfect <jprfct@protonmail.com>
2022-09-10 00:01:40 -0500

    ability to clear all tasks

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ function loadEventListeners() {
   form.addEventListener('submit', addTask);
   // Remove task event
   taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTasks);
 }
 
 // Add Task
@@ -53,4 +55,14 @@ function removeTask(e) {
     }
   }
   
+}
+
+// Clear Tasks
+function clearTasks() {
+  // taskList.innerHTML = '';
+
+  // Faster
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
 }
